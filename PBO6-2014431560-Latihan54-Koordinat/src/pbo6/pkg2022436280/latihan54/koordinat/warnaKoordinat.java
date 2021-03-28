dce6326e17cdd68c1585d0ab671acba55dc8dea5
Zahwa Aulya Sausan Azahra <azahrazahwaulyasa@gmail.com>
2021-03-28 22:32:37 +0700

    first commit

---
+++ PBO6-2014431560-Latihan54-Koordinat/src/pbo6/pkg2022436280/latihan54/koordinat/warnaKoordinat.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6.pkg2022436280.latihan54.koordinat;
+/**
+ *
+ * @author
+ * Nama : Harris Munahar
+ * Kelas : PBO FS112B
+ * NIM : 2022436280
+ */
+
+public class warnaKoordinat {
+    private String namaWarna;
+    
+    public warnaKoordinat(int x, int y, String namaWarna) {
+        this.namaWarna = namaWarna;
+    }
+    public String getNamaWarna() {
+        return namaWarna;
+    }
+    public void setNamaWarna(String namaWarna) {
+        this.namaWarna = namaWarna;
+    }
+}
